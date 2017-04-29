5addc3f2bead4af9e3b0750fe0e47e6a30cdc7b0
Andreydemo <demosoft@bk.ru>
2017-04-29 23:02:20 +0300

    Initial commit

---
+++ Assets/Source/Menu/LoginCanvas.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LoginCanvas : MonoBehaviour
+{
+    public UnityEngine.UI.InputField loginInputField;
+
+    public UnityEngine.UI.InputField passwordInputField;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void Login()
+    {
+        string login = loginInputField.text;
+        string password = passwordInputField.text;
+
+
+        EditorUtility.DisplayDialog("Debug", "Login: " + login + "\nPassword: " + password, "Не ну ок", "Похер");
+    }
+}
